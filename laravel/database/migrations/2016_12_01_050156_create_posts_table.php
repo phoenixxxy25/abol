6abc8d7e9d7b10bbba4084bb567f3952bbc0523e
Astafiev Dmitry <astafiev25dmitry@gmail.com>
2016-12-05 12:19:39 +0200

    nfbb

---
+++ laravel/database/migrations/2016_12_01_050156_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->text('excerpt')->nullable();
+            $table->text('content')->nullable();
+            $table->string('author')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
